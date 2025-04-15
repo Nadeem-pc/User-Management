d05f954fe9822cd552bd0c4addcccd3aebc8c8ea
Nadeem-pc <nadeempc777@gmail.com>
2025-04-15 18:02:02 +0530

    first commit

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import './Login.css'
+
+const Login = () => {
+
+  const [signState, setSignState] = useState("Sign In")
+
+  return (
+    <div className='login-page'>
+        <section> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> 
+            <div className="signin"> 
+                <div className="content"> 
+                    {signState === 'Sign In' ? 
+                        <h2>Sign In</h2> 
+                    : 
+                        <h2>Sign Up</h2>
+                    }
+                    <div className="form"> 
+                        <div className="inputBox"> 
+                            <input type="text" required/> <i>Username</i> 
+                        </div> 
+
+                        <div className="inputBox"> 
+                            <input type="password" required/> <i>Password</i> 
+                        </div> 
+
+                        {signState === 'Sign Up' ? 
+                            <div className="inputBox"> 
+                                <input type="password" required/> <i>Confirm Password</i> 
+                            </div> 
+                        : <></>}
+
+                        {signState === 'Sign In' ? 
+                            <div className="links"> <a href="#">Forgot Password</a> <a onClick={() => setSignState('Sign Up')} style={{cursor:'pointer'}}>Signup</a> </div> 
+                        : <></>}
+
+                        <div className="inputBox"> 
+                            {signState === 'Sign In' ?
+                                <input type="submit" value="Login"/> 
+                            :
+                                <input type="submit" value="Sign Up"/> 
+                            }
+                        </div> 
+                    </div> 
+                </div> 
+            </div> 
+        </section> 
+    </div>
+  )
+}
+
+export default Login
